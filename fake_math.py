7acf672cfdf0879406ae68cf06dd8dd7758a636a
LadyJaine <fullsucsess@gmail.com>
2024-07-18 13:53:54 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+
+def divide(first:int,second:int):
+    if second == 0:
+        print('Ошибка')
+    else:
+        return print(first / second)
---
+++ module_4_1.py
@@ -0,0 +1,9 @@
+from fake_math import divide as df
+from true_math import divide as dt
+df(10,5)
+print()
+df(10,0)
+print()
+dt(5,1)
+print()
+dt(6,0)
